3fb90a8bf3da89f7cbcbeb2948c3d0adaca54dc6
Manthanc007 <manthanchittora@gmail.com>
2021-01-18 18:36:57 +0530

    done

---
+++ function.js
@@ -0,0 +1,10 @@
+
+/** code by webdevtrick ( https://webdevtrick.com ) **/
+var range = $('.input-range'),
+    value = $('.range-value');
+    
+value.html(range.attr('value'));
+ 
+range.on('input', function(){
+    value.html(this.value);
+});
